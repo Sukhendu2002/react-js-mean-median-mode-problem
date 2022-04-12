049b832174f24f7fdf1b1f146feaad8d0b3c08d9
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2022-04-12 15:30:47 +0530

    fix

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ app.get("/dataSet2", (req, res) => {
   }
 });
 
-app.listen(5000 || process.env.PORT, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
